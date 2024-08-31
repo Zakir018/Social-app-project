f9434ac817d246ec616244a040a3febd55e6774c
Zakir018 <zakirgogdare@gmail.com>
2024-08-31 10:15:43 +0500

    push

--- core/social_app/views.py
+++ core/social_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,HttpResponse, get_object_or_404
+from django.shortcuts import render,redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from .models import Profile, Post
@@ -103,6 +103,7 @@ def add_post(request):
 def delete_post(request, pk, page):
     Post.objects.get(id = pk).delete()
     return redirect(page)
+
 
 
 @login_required
